3421eed0a70821d0fa965fd0d9240bc59211a73f
Porchetta <clarantonio98@gmail.com>
2018-10-14 12:19:44 +0200

    [UPDATE] Riorganizzato bot.action, migliorato bot.use per non stampare più undefined, aggiornato settings

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -8,17 +9,17 @@ const positive_negative = "_Insert the value of the positivity or negativity_"
 
 var bloodDonor = require('blood-donor')
 var settings = require("./settings")
-var blood_type = []
+var user_var = {'blood_type': [], 'rh_value': [], 'blood_donor': {}}
 
+console.log("Bot operativo...")
+
+// Managing commands
 bot.command('start', (ctx) => ctx.replyWithMarkdown(settings.start_msg))
 bot.command('help', (ctx) => ctx.replyWithMarkdown(settings.help_msg))
-
-//bot.command('blood', (ctx) => ctx.replyWithMarkdown("_Insert your kind of blood_"))
-
 bot.command('blood', (ctx) => {
   return ctx.replyWithMarkdown('_Insert your kind of blood_',
     Markup.inlineKeyboard([
-      Markup.callbackButton('0', '0'),
+      Markup.callbackButton('0', 'O'),
       Markup.callbackButton('A', 'A'),
       Markup.callbackButton('B', 'B'),
       Markup.callbackButton('AB', 'AB')
@@ -26,44 +27,34 @@ bot.command('blood', (ctx) => {
   )
 })
 
-bot.action(/(.+)/, (ctx, next) => {
-  let match = ctx.match[0];
-
-  switch (match) {
-    case '0':
-    case 'A':
-    case 'B':
-    case 'AB':
-      blood_type[ctx.chat.id] = match
-      ctx.answerCbQuery(`Your kind of blood is set on: ${match}`).then(() => next())
-      ctx.editMessageText(positive_negative,
-        Markup.inlineKeyboard([
-          Markup.callbackButton('Positive', 'Positive'),
-          Markup.callbackButton('Negative', 'Negative'),
-        ]).extra()
-      )
-      break;
-    case 'Positive':
-    case 'Negative':
-      ctx.answerCbQuery(`Your kind of blood is: ${blood_type[ctx.chat.id]} Rh. ${match}`).then(() => next())
-      break;
-    default:
-      ctx.answerCbQuery('I can not set your kind of blood').then(() => next())
-      break;
-  }
-  return "22"
+// Managing inline query
+bot.action(/([O|A|B|AB])/, (ctx, next) => {
+  user_var.blood_type[ctx.chat.id] = ctx.match[0]
+  ctx.answerCbQuery(`Your kind of blood is set on: ${ctx.match[0]}`).then(() => next())
+  ctx.editMessageText(settings.positive_negative,
+    Extra.markdown().markup((m) => m.inlineKeyboard([
+      Markup.callbackButton('+', '+'),
+      Markup.callbackButton('-', '-'),
+    ])
+  ))
 })
 
-// bloodDonor(blood_type, function (err, data) {
-//       console.log(data);
-// });
+bot.action(/(\+|\-)/, (ctx, next) => {
+  user_var.rh_value[ctx.chat.id] = ctx.match[0]
+  ctx.answerCbQuery(`Your kind of blood is: ${user_var.blood_type[ctx.chat.id]} Rh. ${ctx.match[0]}`).then(() => next())
+  bloodDonor(`${user_var.blood_type[ctx.chat.id]}${user_var.rh_value[ctx.chat.id]}ve`, function (err, data) {
+    user_var.blood_donor = data
+    console.log(data)
+  })
+})
 
 bot.catch((err) => {
   console.log('Ooops', err)
 })
 
 bot.use((ctx) => {
-  console.log(ctx.message)
+  if(ctx.updateType != 'callback_query')
+    console.log('Messaggio ricevuto: ', ctx.message)
 })
 
 bot.startPolling()
--- settings.js
+++ settings.js
@@ -1,6 +1,9 @@
-var start_msg = "*Welcome in Krew!*\n\nWith this Bot you will be able to understand "+
+var start_msg = "*Welcome in Krew!*\n\nWith this Bot you wwill be able to understand "+
                 "more information about your blood through the command /blood"
 var help_msg = "_I need your help_"
 
+var positive_negative = "_Insert the value of the positivity or negativity_"
+
 exports.start_msg = start_msg
 exports.help_msg = help_msg
+exports.positive_negative = positive_negative
