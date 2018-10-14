346f6924eabb6ae7b0b156625dcfd65f3fe8e3df
Francesco Coppola <f.coppola1998@gmail.com>
2018-10-14 12:32:07 +0200

    [FIX] Correzione messaggio di testo

--- settings.js
+++ settings.js
@@ -1,7 +1,6 @@
-var start_msg = "*Welcome in Krew!*\n\nWith this Bot you wwill be able to understand "+
+var start_msg = "*Welcome in Krew!*\n\nWith this Bot you will be able to understand "+
                 "more information about your blood through the command /blood"
 var help_msg = "_I need your help_"
-
 var positive_negative = "_Insert the value of the positivity or negativity_"
 
 exports.start_msg = start_msg
